29bfb5235661027dd32a6a4cb8c8b56f24f523c6
ahals <ahalyag96@gmail.com>
2019-09-01 10:23:15 +0530

    Layout

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,30 +1,30 @@
 require 'test_helper'
 
-class StaticPagesControllerTest < ActionController::TestCase
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 def setup
   @BaseTitle = "Ruby on Rails Tutorial Sample App"
 end
 
   test "should get home" do
-    get :home
+    get root_path
     assert_response :success
     assert_select  "title", "#{@BaseTitle}"
   end
 
   test "should get help" do
-    get :help
+    get help_path
     assert_response :success
     assert_select  "title", "Help | #{@BaseTitle}"
   end
 
   test "should get about" do
-    get :about
+    get about_path
     assert_response :success
     assert_select  "title", "About | #{@BaseTitle}"
   end
 
   test "should get contact" do
-    get :contact
+    get contact_path
     assert_response :success
     assert_select  "title", "Contact | #{@BaseTitle}"
   end
